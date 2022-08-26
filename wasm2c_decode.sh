f4cc71a173842f7c1d2cf7eedd0a5d30572937ae
Jake Goldman <jgoldman@caltech.edu>
2022-08-26 13:56:33 -0400

    clean repo - remove out folder

--- wasm2c_decode.sh
+++ wasm2c_decode.sh
@@ -2,7 +2,7 @@
 set -e
 
 help() {
-	echo "Configure decode.c and WAMR compiler with libpng WASM target."
+	echo "Compile decode.c to wasm and re-compile back to c."
 	echo 
 	echo "Syntax: bash wasm_decode.sh [-h|s]"
 	echo "options:"
@@ -23,3 +23,17 @@ done
 
 SCRIPT=$(readlink -f "$0")
 SCRIPT_PATH=$(dirname "$SCRIPT")
+
+# Assume SIMD disabled for now; TODO add simd functionality
+
+# Compile decode.c to wasm 
+LDFLAGS="-Wl,--export-all -Wl,--growable-table" \
+${WASI_SDK_PATH}/bin/clang \
+--sysroot ${WASI_SDK_PATH}/share/wasi-sysroot \
+-I${WASI_SDK_PATH}/share/wasi-sysroot/include/libpng16 \
+-L${WASI_SDK_PATH}/share/wasi-sysroot/lib \
+-o out/decode.wasm \
+decode.c \
+-lpng16 -lz 
+
+# Compile decode.wasm to c
